35aa086ef8061b650874b2f92e593fb3056e7805
Dishank Parmar <dishank.parmar@gmail.com>
2024-09-26 17:45:26 +0530

    signup page changes

--- Scripts/data/UserInfoData.js
+++ Scripts/data/UserInfoData.js
@@ -4,11 +4,13 @@ if(!UserInfo) {
   UserInfo = [
     {
       fullName: 'Dishank Parmar',
+      email: 'dishank003@gmail.com',
       userName: 'dishank.parmar',
       password: 'dishank1234'
     },
     {
       fullName: 'Zaisha Parmar',
+      email: 'zaisha28@gmail.com',
       userName: 'zaisha.parmar',
       password: 'zaisha1234'
     },
--- Scripts/new2.js
+++ Scripts/new2.js
@@ -1,3 +1,42 @@
 import { UserInfo } from "./data/UserInfoData.js";
 
-console.log(UserInfo);
+let username = document.querySelector('.username-text-box');
+let email = document.querySelector('.email-text-box');
+let password = document.querySelector('.password-text-box');
+let password2 = document.querySelector('.password2-text-box');
+let isWrong= true;
+
+username.addEventListener('input', () => {
+  for(let i = 0; i<UserInfo.length; i++) {
+    if(username.value === UserInfo[i].userName) {
+      document.querySelector('.username-exists-text').innerHTML= 'Account with this username already exists';
+      break;
+    } else {
+      document.querySelector('.username-exists-text').innerHTML= null;
+    }
+  }
+})
+
+email.addEventListener('input', () => {
+  for(let i = 0; i<UserInfo.length; i++) {
+    if(email.value === UserInfo[i].email) {
+      document.querySelector('.email-exists-text').innerHTML= 'Account with this email already exists';
+      break;
+    } else {
+      document.querySelector('.email-exists-text').innerHTML= null;
+    }
+  }
+})
+
+password2.addEventListener('input', () => {
+  if(password2.value === password.value) {
+    document.querySelector('.password-match-text').innerHTML = 'Password matches';
+  }
+  else if ((password2.value === '')|| (password.value === '')) {
+    document.querySelector('.password-match-text').innerHTML = null;
+  }
+  else {
+    document.querySelector('.password-match-text').innerHTML = "Password doesn't match";
+  }
+})
+
